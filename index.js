44938c37e7336a156bd171d3b31e63738def8189
Tejashree J <81523913+Tejashree-J@users.noreply.github.com>
2021-06-14 14:00:35 +0530

    Add files via upload
    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+let buttons = document.querySelectorAll(".drum").length;
+for(var i=0; i<buttons; i++){
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    let buttonInnerHtml = this.innerHTML;
+    makeSound(buttonInnerHtml);
+    buttonAnimation(buttonInnerHtml);
+});
+
+}  
+
+document.addEventListener('keydown',function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            let tom4 = new Audio('sounds/tom-4.mp3'); 
+            tom4.play();
+            break;
+        case "a" :
+            let tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "s" :
+            let kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        case "d" :
+            let tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        
+        case "j" :
+            let crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "k" :
+            let tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case "l" :
+            let snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        default: console.log(buttonInnerHtml);
+        break;
+    }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    
+    setTimeout(() => {
+        activeButton.classList.remove("pressed");
+    }, 150);
+}
